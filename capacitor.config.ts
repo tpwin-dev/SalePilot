cfbc821ca2cdea2b0871f3743374fee2cc173f2f
ThetPaingWin <thetpaingwin.dev@gmail.com>
2026-09-21 14:16:41 +0630

    android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.salepilot.app',
+  appName: 'SalePilot',
+  webDir: 'dist',
+}
+
+export default config
